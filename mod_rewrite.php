d2697bb4dfeea57117eda300c64cbe1a3f42d78c
guerno <test1234>
2014-11-02 13:04:27 +0100

    error removed fatal

--- mod_rewrite.php
+++ mod_rewrite.php
@@ -10,5 +10,4 @@ echo "hello world";
 echo "<br/> ok, this is nice, probably something was wrong with the irgendwass";
 echo "<br/> cool this is an extra step which is complete!";
 echo "<br/> Hello Thomas, just playing around with the google app engine. Looking fine!";
-echo "update updatle";
-lets write a php error $asdfdsaf;
+echo "<br/> error removed";
