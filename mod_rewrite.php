65a10f9bed6a138c11efa53b5a762c9187a97d47
guerno <test1234>
2014-11-02 13:01:58 +0100

    php error added

--- mod_rewrite.php
+++ mod_rewrite.php
@@ -11,3 +11,4 @@ echo "<br/> ok, this is nice, probably something was wrong with the irgendwass";
 echo "<br/> cool this is an extra step which is complete!";
 echo "<br/> Hello Thomas, just playing around with the google app engine. Looking fine!";
 echo "update updatle";
+lets write a php error $asdfdsaf;
